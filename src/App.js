a63e7d6eedc8bb511fdcb66e4fab4f0d29dcb116
ppoblocki <patryk.drv@gmail.com>
2020-04-09 09:19:15 +0200

    Add failing test

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my frontend App!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ppoblocki/my-frontend-app-docker"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Patryk Pobłocki
         </a>
       </header>
     </div>
